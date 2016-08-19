b2f23ce7faeff5f9cfc09e199f8d3d6d075fca25
fangqi <fangqi@xywy.com>
2016-08-19 10:36:59 +0800

    commit SimpleStack

---
+++ app/src/main/java/com/jackfangqi/commonutil/utils/SimpleStack.java
@@ -0,0 +1,30 @@
+package com.jackfangqi.commonutil.utils;
+
+import java.util.LinkedList;
+
+/**
+ * Compiler: Android Studio
+ * Project: CommonUtil
+ * Author: Jack Fang
+ * Email: fangqi@xywy.com
+ * Date: 2016/8/19 10:33
+ */
+public class SimpleStack {
+    private LinkedList<Object> list;
+
+    public SimpleStack() {
+        this.list = new LinkedList<>();
+    }
+
+    public void add(Object o) {
+        this.list.push(o);
+    }
+
+    public Object get() {
+        return this.list == null ? null : this.list.pop();
+    }
+
+    public int size() {
+        return this.list == null ? 0 : this.list.size();
+    }
+}

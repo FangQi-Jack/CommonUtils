57be70a3c3fe7daf788f3f285f8c7f9b779f024b
fangqi <fangqi@xywy.com>
2016-08-24 10:39:50 +0800

    commit

--- commonutil/src/main/java/com/jackfangqi/commonutil/MainActivity.java
+++ commonutil/src/main/java/com/jackfangqi/commonutil/MainActivity.java
@@ -1,7 +1,11 @@
 package com.jackfangqi.commonutil;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jackfangqi.commonutil.utils.LogUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogUtil.d("onCreate->");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LogUtil.d("onStart->");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LogUtil.d("onResume->");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LogUtil.d("onPause->");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LogUtil.d("onStop->");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        LogUtil.d("onRestart->");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LogUtil.d("onDestroy->");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        LogUtil.d("onSaveInstanceState->");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        LogUtil.d("onRestoreInstanceState->");
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        LogUtil.d("onConfigurationChanged->");
     }
 }
